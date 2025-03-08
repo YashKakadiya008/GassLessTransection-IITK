5d8b2a1190b0e4ad5b6080ac34be8cbb71cd8eda
YashKakadiya008 <159806869+YashKakadiya008@users.noreply.github.com>
2025-03-08 12:54:23 +0530

    second

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,26 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = "surprise tenant juice crystal artwork pond inform vapor click caught rally tunnel"; // Replace with your Metamask mnemonic
 
 module.exports = {
+  hedera_testnet: {
+      provider: () => {
+        return new HDWalletProvider({
+          privateKeys: [privateKey],
+          providerOrUrl: "https://testnet.hashio.io/api",
+          pollingInterval: 15000,
+          networkCheckTimeout: 1000000,
+          timeoutBlocks: 500,
+        });
+      },
+      network_id: "296",
+      gas: 8000000,
+      gasPrice: 390000000000,
+      chainId: 296,
+      confirmations: 3,
+      skipDryRun: true,
+      timeoutBlocks: 500,
+      networkCheckTimeout: 1000000,
+      deploymentPollingInterval: 15000,
+    },
   networks: {
     sepolia: {
       provider: () => new HDWalletProvider(mnemonic, "https://eth-sepolia.g.alchemy.com/v2/8SpOm-MvRcOKtk8cIQKN9e1w9tw_yNTh"),
